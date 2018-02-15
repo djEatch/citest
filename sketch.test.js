20d43c6cf3665d6f18b8ec6951a11bc5e7dd7cdf
djEatch <duncan_eatch@hotmail.com>
2018-02-15 13:02:33 +0000

    initial comit for testings

---
+++ sketch.test.js
@@ -0,0 +1,11 @@
+const sum = require('./sketch');
+
+// test('adding 1 to 2 to equal 3', () => {
+//     expect(sum(1,2)).toBe(3);
+// });
+
+test('adding 1 to 2 to equal 3',);
+
+function sumTest() {
+    expect(sum(1,2)).toBe(3);
+}
